cda7722f9f85f75bb6f037cce983723f56636e64
MoKholy <76899713+MoKholy@users.noreply.github.com>
2026-05-19 11:29:33 +0400

    Add eval script, examples  and eval readme

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # Arabic Sentence Segmentation Shared Task 2026
+
+## Eval
+We define Sentence segmentation as a **binary** token classification task. We use the following metrics:
+
+* **Boundary Precision (P):** The percentage of predicted sentence boundaries that match a reference sentence boundary. Higher precision means fewer false boundary insertions.
+* **Boundary Recall (R):** The percentage of reference sentence boundaries that are correctly predicted. Higher recall means fewer missed sentence boundaries.
+* **Boundary F1 (F1):** The harmonic mean of boundary precision and boundary recall. It provides a single score that balances over-segmentation and under-segmentation errors.
+
+We compute these metrics per document, and average across all documents in the split.
+
+## Setup
+
+You can set up the environment using either **Conda** or Python’s built-in **venv**.
+
+### Option 1: Conda
+
+Create and activate a new Conda environment:
+
+```bash
+conda create -n sent-seg python=3.10
+conda activate sent-seg
+```
+
+### Option 2: venv
+
+Create a virtual environment:
+```bash
+python -m venv .venv
+```
+
+Activate environment
+```bash
+# Mac/linux
+source .venv/bin/activate
+
+# Windows
+.venv\Scripts\activate
+```
+
+Install required dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+## Running Evaluation
+
+Example inputs and outputs are available in the `examples/` subdirectory.
+
+### Example command:
+```bash
+python scripts/eval.py \
+    --task-name NoPnx-NP \
+    --prediction-file-path examples/NoPnx-NP_example.csv \
+    --output-file-path examples/ \
+    --split test
+```
+
+The evaluation results are saved as:
+```bash
+{task_name}_{split}.json
+```
---
+++ scripts/eval.py
@@ -0,0 +1,101 @@
+import json
+from argparse import ArgumentParser
+from typing import Dict, FrozenSet, List
+
+import numpy as np
+import pandas as pd
+from datasets import load_dataset
+from sklearn.metrics import f1_score, precision_score, recall_score
+
+TASK_TO_DATASET_NAMES: FrozenSet[str] = {
+    "Pnx-PA": "MBZUAI/AraSeg-2026-Shared-Task-Pnx-PA",
+    "Pnx-NP": "MBZUAI/AraSeg-2026-Shared-Task-Pnx-NP",
+    "NoPnx-PA": "MBZUAI/AraSeg-2026-Shared-Task-NoPnx-PA",
+    "NoPnx-NP": "MBZUAI/AraSeg-2026-Shared-Task-NoPnx-NP"
+}
+
+SPLITS: FrozenSet[str] = frozenset(["train", "dev", "test"])
+
+def load_data_from_hf(task_name, split=None):
+    assert task_name in TASK_TO_DATASET_NAMES, f"Task {task_name} not found in the dataset names"
+    assert split in SPLITS, f"Split {split} not found in the splits"
+    if split is None:
+        dataset = load_dataset(TASK_TO_DATASET_NAMES[task_name])
+        return dataset
+    else:
+        dataset = load_dataset(TASK_TO_DATASET_NAMES[task_name], split=split)
+        return dataset
+
+def convert_binary_string_to_list(binary_string: str) -> List[int]:
+    return [int(char) for char in binary_string]
+
+def load_predictions(prediction_file_path: str) -> Dict[str, List[int]]:
+    df = pd.read_csv(prediction_file_path, header=0)
+    docs = {}
+    for index, row in df.iterrows():
+        doc_id = row["Document ID"]
+        labels = convert_binary_string_to_list(row["Predictions"])
+        docs[doc_id] = labels
+    return docs
+
+def compute_metrics(gold_documents: Dict[str, List[int]], predicted_documents: Dict[str, List[int]]):
+    assert len(gold_documents) == len(predicted_documents), "Gold and predicted documents must have the same length"
+    doc_metrics = {}
+    for doc in gold_documents:
+        gold_labels = gold_documents[doc]
+        pred_labels = predicted_documents[doc]
+        assert len(gold_labels) == len(pred_labels), "Gold and predicted labels must have the same length"
+        doc_metrics[doc] = {
+            'precision': precision_score(gold_labels, pred_labels, average='binary'),
+            'recall': recall_score(gold_labels, pred_labels, average='binary'),
+            'f1': f1_score(gold_labels, pred_labels, average='binary')
+        }
+    macro_precision = np.mean([metric['precision'] for metric in doc_metrics.values()])
+    macro_recall = np.mean([metric['recall'] for metric in doc_metrics.values()])
+    macro_f1 = np.mean([metric['f1'] for metric in doc_metrics.values()])
+    return {
+        'macro_precision': macro_precision,
+        'macro_recall': macro_recall,
+        'macro_f1': macro_f1
+    }
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--task-name", type=str, required=True, choices=TASK_TO_DATASET_NAMES.keys(), help="The name of the task to evaluate")
+    parser.add_argument("--prediction-file-path", type=str, required=True, help="The path to the prediction file")
+    parser.add_argument("--output-file-path", type=str, required=True, help="The path to the output file")
+    parser.add_argument("--split", type=str, required=False, default="dev", choices=SPLITS, help="The split to evaluate on")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    task_name = args.task_name
+    prediction_file_path = args.prediction_file_path
+    output_file_path = args.output_file_path
+    split = args.split
+
+    print("-"*100)
+    print(f"Prediction file path: {prediction_file_path}")
+    print(f"Output file path: {output_file_path}")
+    print(f"Task name: {task_name}")
+    print(f"Split: {split}")
+    print("-"*100)
+    print(f"Evaluating {task_name} on {split} split")
+
+    # load data
+    data = load_data_from_hf(task_name, split)
+    gold_documents = {doc["doc_id"]: doc["labels"] for doc in data}
+    predicted_documents = load_predictions(prediction_file_path)
+    assert set(gold_documents.keys()) == set(predicted_documents.keys()), f"Gold and predicted documents must have the same keys. Missing gold documents: {set(gold_documents.keys()) - set(predicted_documents.keys())}"
+    metrics = compute_metrics(gold_documents, predicted_documents)
+    metrics = {
+        "macro_precision": metrics["macro_precision"].item(),
+        "macro_recall": metrics["macro_recall"].item(),
+        "macro_f1": metrics["macro_f1"].item()
+    }
+    print(f"Metrics: {metrics}")
+    print("-"*100)
+    output_file_name = f"{task_name}_{split}.json"
+    with open(f"{output_file_path}/{output_file_name}", "w") as f:
+        json.dump(metrics, f, indent=4)
+    print(f"Metrics saved to {output_file_path}/{output_file_name}")
